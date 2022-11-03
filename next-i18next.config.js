e6cb31da193968454a18fbca54f4583eec41591c
Caio Quintas <caioquintassantiago@gmail.com>
2022-11-03 11:39:00 -0300

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -7,3 +7,22 @@ module.exports = {
 	},
 	localePath: path.resolve("./public/locales"),
 };
+
+* petme
+spectre/luna cloud
+react native
+google api (maps)
+react auth
+
+99 like initial interface
+
+- find pets
+- pet transport/carrier
+- luna ai for help tracking missing pets
+- pin in map for vets
+- mini social midia for pet owners and enthusiasts (photo/video/blog posts)
+- pet harm complaint
+- adoption tab
+- chat with stickers
+- official blog/news tab using mini social midia components
+- pet birth registration
