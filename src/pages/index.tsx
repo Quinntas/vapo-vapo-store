464154c54099d35bf2b9c3b23d4f80b0d32ade95
Caio Quintas <caioquintassantiago@gmail.com>
2022-11-17 20:03:43 -0300

    Routing to search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,8 +16,14 @@ import { homeThreeMasonryBanner as masonryBanner } from '@framework/static/banne
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { ROUTES } from '@utils/routes';
 import { GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    { router.push("/search") }
+  }, []);
   return (
     <>
       <BannerBlock data={masonryBanner} />
