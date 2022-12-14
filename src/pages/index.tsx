6dc7e59540e135939413068f98093d6b1d8a1321
Caio Quintas <caioquintassantiago@gmail.com>
2022-12-14 10:49:15 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,11 @@ import BannerWithProducts from '@containers/banner-with-products';
 import BannerBlock from '@containers/banner-block';
 import Divider from '@components/ui/divider';
 import ProductsFeatured from '@containers/products-featured';
-import BannerSliderBlock from '@containers/banner-slider-block';
 import ExclusiveBlock from '@containers/exclusive-block';
 import Subscription from '@components/common/subscription';
 import NewArrivalsProductFeed from '@components/product/feeds/new-arrivals-product-feed';
-import {homeThreeBanner as banner} from '@framework/static/banner';
 import {homeThreeMasonryBanner as masonryBanner} from '@framework/static/banner';
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
-import {ROUTES} from '@utils/routes';
 import {GetStaticProps} from 'next';
 
 export default function Home() {
@@ -24,20 +21,7 @@ export default function Home() {
                 <CategoryBlock sectionHeading="Procure por Categoria" type="rounded"/>
                 <ProductsFeatured sectionHeading="Produtos em Destaque"/>
             </Container>
-            <BannerSliderBlock/>
             <Container>
-                <BannerCard
-                    key={`banner--key${banner[0].id}`}
-                    banner={banner[0]}
-                    href={`${ROUTES.COLLECTIONS}/${banner[0].slug}`}
-                    className="mb-12 lg:mb-14 xl:mb-16 pb-0.5 lg:pb-1 xl:pb-0"
-                />
-                <BannerCard
-                    key={`banner--key${banner[1].id}`}
-                    banner={banner[1]}
-                    href={`${ROUTES.COLLECTIONS}/${banner[1].slug}`}
-                    className="mb-12 lg:mb-14 xl:mb-16 pb-0.5 lg:pb-1 xl:pb-0"
-                />
                 <NewArrivalsProductFeed/>
                 <ExclusiveBlock/>
                 <BannerWithProducts
